461bdd7c540f02e2a114f60460b6b85811990fb7
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2022-11-22 18:26:54 +0100

    Updating some stylng

--- web-content/main.js
+++ web-content/main.js
@@ -688,3 +688,19 @@ function deleteCookie(name) {
     var expires = "expires=" + d.toGMTString();
     document.cookie = name + "=;" + expires + ";path=/";
 }
+// =====GETTING ITEMS FROM COOKIES=====
+function getCookie(name) {
+    var cname = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(cname) == 0){
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
